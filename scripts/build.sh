66d22aef51fc9607f0dbbed7c4a424eaa49b8477
Rizean <rizean@gmail.com>
2020-11-02 19:05:26 -0500

    updated build script to use twee3

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-twee2 build YoungMaria8.0.0_Offline.tw2 YoungMaria8.0.0_Offline.tw2.html --format=./twinejs/story-formats/sugarcube-2.31.1
-node ./scripts/postBuild.js
+node ../twee3/src/index.js build YoungMaria8.0.0_Offline.tw2 YoungMaria8.0.0_Offline.tw2.html sugarcube-2.31.1
 
 # git clone https://github.com/klembot/twinejs.git
+# node ../twee3/src/index.js decompile "dist/Young Maria 8.3.0 (Online).html" YoungMaria8.3.0.Online.tw2
