1806f01928226d8047a394621ddd7dadab86486a
Rizean <rizean@gmail.com>
2020-11-08 18:06:47 -0500

    cleanup

--- src/js/GameObject.js
+++ src/js/GameObject.js
@@ -12,12 +12,3 @@ class GameObject extends PersistentObject {
 
 window.GameObject = GameObject
 
-
-// 	<!--<div id="DollChest">[img[String.format('pics/PaperDoll/UpperBody/Chest/' + $dollChestSize + $dollNipplesClothes + '.png')]]</div>-->
-// $mainChar.look.erotic_accessories.body.name
-//    <<set $dollBra = $mainChar.look.lingerie.bra.name>>
-//    <<set $dollNipplesClothes = $mainChar.look.erotic_accessories.body.name>>
-// 	<div id="DollBra">[img[String.format('pics/PaperDoll/UpperBody/Bra/' + $dollBra + $dollChestSizeClothes + '.png')]]</div>
-// D:\twine\Young-Maria\pics\PaperDoll\UpperBody\Bra\DefaultBraD.png
-// <div id="DollMainClothes">[img[String.format('pics/PaperDoll/UpperBody/MainClothes/' + $dollMainClothes + $dollChestSizeClothes + '.png')]]</div>
-
